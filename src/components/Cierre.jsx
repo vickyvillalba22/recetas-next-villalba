da19a324f3374ecd0522c0eed9a54ef4fc8b93ec
vickyvillalba22 <villalbavictoria120@gmail.com>
2026-05-06 11:27:41 -0300

    grid cierre

--- src/components/Cierre.jsx
+++ src/components/Cierre.jsx
@@ -4,20 +4,29 @@ import Image from 'next/image'
 const Cierre = ({items}) => {
 
   console.log(items);
-  
 
+  if (!items || items.length < 3) return <p>Cargando...</p>
+  
   return (
-    <section className='flex h-[90vh] justify-between items-center'>
+    <section className='flex h-[90vh] justify-evenly items-center'>
 
       <div className='flex flex-col w-[40%] gap-4'>
           <h4>¡Gracias por ver!</h4>
           <p>Esperamos que estas recetas te resulten útiles y puedas cocinarselas a quien quieras! Porque no hay nada mejor que una rica comida.</p>
       </div>
 
-      <div className="grid grid-cols-2 grid-rows-5 gap-4">
-        <Image src={items[0].image} width={100} height={100} alt={items[0].name} className="row-span-2" />
-        <Image src={items[1].image} width={100} height={100} alt={items[1].name} className="row-span-3 col-start-1 row-start-3" />
-        <Image src={items[2].image} width={100} height={100} alt={items[2].name} className="row-span-5 col-start-2 row-start-1" />
+      <div className="grid grid-cols-2 grid-rows-5 gap-4 h-[60vh] w-[35%]">
+        <div className="relative row-span-2">
+          <Image src={items[0].image} fill alt={items[0].name} className="object-cover rounded-2xl" loading='eager'/>
+        </div>
+
+        <div className="relative row-span-3 col-start-1 row-start-3">
+          <Image src={items[1].image} fill alt={items[1].name} className="object-cover rounded-2xl" loading='eager' />
+        </div>
+
+        <div className="relative row-span-5 col-start-2 row-start-1">
+          <Image src={items[2].image} fill alt={items[2].name} className="object-cover rounded-2xl" loading='eager' />
+        </div>
       </div>
 
 
