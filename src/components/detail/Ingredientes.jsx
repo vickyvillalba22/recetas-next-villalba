24129218e7a807a4c2493e2a68e5dbfc8a78c5c3
vickyvillalba22 <villalbavictoria120@gmail.com>
2026-05-10 18:36:47 -0300

    css ingredients and instructions

--- src/components/detail/Ingredientes.jsx
+++ src/components/detail/Ingredientes.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
+import { Icon } from '@iconify/react'
 
 const Ingredientes = ({item}) => {
   return (
-    <section className='row-span-3 col-start-2 row-start-3 border-2 border-(--color-medio) rounded-2xl'>
-        
-        <h4>Ingredients</h4>
 
-        <ul className='list-none flex flex-col gap-4'>
+    <section className='row-span-3 col-start-2 row-start-3 border-2 border-(--color-medio) rounded-2xl p-8 flex flex-col gap-4 relative overflow-hidden'>
+        
+        <h4 className='text-2xl font-bold'>Ingredients</h4>
+
+        <ul className='list-disc gap-4 pl-4 grid grid-cols-2'>
             {item.ingredients.map((ing, index)=>(
                 <li key={index}>{ing}</li>
             ))}
         </ul>
+
+        <Icon 
+          icon="hugeicons:cookie"
+          width="150"
+          height="150"
+          className="absolute top-[65%] right-[5%] opacity-50 text-(--color-medio)"
+        />
 
     </section>
   )
--- src/components/detail/Instrucciones.jsx
+++ src/components/detail/Instrucciones.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
+import { Icon } from '@iconify/react'
 
 const Instrucciones = ({item}) => {
 
     return (
-        <section className='row-span-5 col-start-3 row-start-1 border-2 border-(--color-acento) rounded-2xl'>
+        <section className='row-span-5 col-start-3 row-start-1 border-2 border-(--color-acento) rounded-2xl flex flex-col gap-4 relative overflow-hidden p-8'>
             
-            <h4>Instructions</h4>
+            <h4 className='text-2xl font-bold'>Instructions</h4>
 
             <ul className='list-none flex flex-col gap-4'>
                 {item.instructions.map((inst, index)=>(
-                    <li key={index}><strong>{index}</strong>{inst}</li>
+                    <li key={index}><strong>{index}) </strong>{inst}</li>
                 ))}
             </ul>
+
+            <Icon 
+                icon="hugeicons:cook-book"
+                width="150"
+                height="150"
+                className="absolute top-[75%] right-[-20] opacity-40 text-(--color-acento) rotate-20"
+            />
             
         </section>
     )
